35da3dac3eaac3ede4ad273ca82ceb11aa6dc359
FF02478 <91719201+FF02478@users.noreply.github.com>
2022-07-05 13:44:20 +0900

    MainCameraMoveを追加

--- Assets/Scripts/MainCameraMove.cs
+++ Assets/Scripts/MainCameraMove.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class MainCameraMove : MonoBehaviour
 {
+    GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 playerPos = this.player.transform.position;
+        
+        if(transform.position.y<playerPos.y)
+            transform.position = new Vector3(
+            transform.position.x, playerPos.y, transform.position.z);
+        else
+        {
+            transform.position = new Vector3(
+                transform.position.x, transform.position.y, transform.position.z);
+        }
         
     }
 }

c0aa87d361f24943fc94e55ca1ba4070d12a4db1
FF02478 <91719201+FF02478@users.noreply.github.com>
2022-07-13 12:43:46 +0900

    enterでスタートするように変更

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -1,11 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameStart : MonoBehaviour
 {
-    public void ButtonClicked () 
+
+    private void TransitionToMain()
     {
         SceneManager.LoadScene("Main");
+    }
+    public void ButtonClicked () 
+    {
+       TransitionToMain();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            TransitionToMain();
+        }
     }
 }
